c7e67e52349b367989521a6e71d965e3ee64a452
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-05-08 17:03:07 +0530

    Time: 14 ms (18.61%) | Memory: 13.2 MB (40.02%) - LeetSync

--- string/72-edit-distance/edit-distance.cpp
+++ string/72-edit-distance/edit-distance.cpp
@@ -10,13 +10,13 @@ public:
         for(int i=0;i<=n;i++){
             dp[i][0] = i;
         }
-        
-        for(int i=0;i<=m;i++){
-            dp[0][i] = i;
+
+        for(int j=0;j<=m;j++){
+            dp[0][j] = j;
         }
 
         for(int i=1;i<=n;i++){
-            for(int j = 1;j<=m;j++){
+            for(int j=1;j<=m;j++){
 
                 if(word1[i-1] == word2[j-1]){
                     dp[i][j] = dp[i-1][j-1];
@@ -24,6 +24,7 @@ public:
                 else
                 {
                     dp[i][j] = 1+ min({
+
                         dp[i-1][j],
                         dp[i][j-1],
                         dp[i-1][j-1]
