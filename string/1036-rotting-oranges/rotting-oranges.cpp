efdf1e01237957d96a1aa28422609edad38d8811
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-05-09 23:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (85.65%) - LeetSync

--- string/1036-rotting-oranges/rotting-oranges.cpp
+++ string/1036-rotting-oranges/rotting-oranges.cpp
@@ -1,63 +1,56 @@
 class Solution {
 public:
-
-    
-      
     int orangesRotting(vector<vector<int>>& grid) {
 
         int n = grid.size();
         int m = grid[0].size();
 
-       queue<pair<int,int>>que;
+        int fresh = 0;
 
-       int fresh = 0;
+        queue<pair<int,int>>que;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-
                 if(grid[i][j] == 2){
                     que.push({i,j});
                 }
-                else if(grid[i][j] == 1){
+                else if(grid[i][j] == 1)
                     fresh++;
-                }
+            
             }
         }
 
-        vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
+        vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
 
-        int min=0;
+        int mi = 0;
 
-        while(!que.empty() && fresh > 0)
-        {
+        while(!que.empty() && fresh > 0){
 
-            int q = que.size();
+            int num = que.size();
 
-            for(int i=0;i<q;i++){
+            for(int i=0;i<num;i++){
 
-                auto[dx,dy] = que.front();
+                int x = que.front().first;
+                int y = que.front().second;
                 que.pop();
 
+                for(auto &[a,b]:dir){
+                    int nx = x+a;
+                    int ny = y+b;
 
-
-                for(auto &[x,y]:dir){
-                    int nx = dx+x;
-                    int ny = dy+y;
-
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] == 1){
-                            grid[nx][ny] = 2;
-                            que.push({nx,ny});
-                            fresh--;
-                        }
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 1){
+                        grid[nx][ny] = 2;
+                        fresh--;
+                        que.push({nx,ny});
+                    }
                 }
-
             }
 
-                min++;
-
+            mi++;
         }
 
-            return (fresh == 0) ? min : -1;
+        return fresh == 0?mi:-1;
+    
+        
     }
-
 };
