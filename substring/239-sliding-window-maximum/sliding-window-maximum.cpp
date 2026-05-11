8d54c745c72cbc1b0a5922461685d627d79d2b81
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-05-12 01:48:39 +0530

    Time: 19 ms (75.00%) | Memory: 139 MB (88.44%) - LeetSync

--- substring/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ substring/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,52 +1,29 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-        deque<int> dq;
+
         int n = nums.size();
-        vector<int> ans;
 
-        for(int i=0;i<n;i++){
-            while(!dq.empty() && dq.front() <= i - k) {
-                dq.pop_front();
-            }
+        vector<int>res;
 
-            while(!dq.empty() && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
-            }
-            dq.push_back(i);
-            if(i >= k - 1) {
-                ans.push_back(nums[dq.front()]);
-            }
+        deque<int>que;
+
+      for(int i=0;i<n;i++){
+
+        while(!que.empty() && que.front() <= i-k){
+            que.pop_front();
         }
-        return ans;
-    }
-};
-        int n = nums.size();
 
-        vector<int> res;
+        while(!que.empty() && nums[que.back()] <= nums[i]){
+            que.pop_back();
+        }
 
-        deque<int> que;
+         que.push_back(i);
 
-        for (int i = 0; i < n; i++) {
-
-            while (!que.empty() && que.front() <= i - k) {
-                que.pop_front();
-            }
-
-            while (!que.empty() && nums[que.back()] <= nums[i]) {
-                que.pop_back();
-            }
-
-            que.push_back(i);
-
-            if (i >= k - 1) {
+            if(i >= k - 1) {
                 res.push_back(nums[que.front()]);
             }
-        }
-        return res;
+      }
+       return  res;
     }
 };
