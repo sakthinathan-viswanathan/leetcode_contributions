dd75d00e5731cf74a016bbe4ba255c4ba00323e9
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-05-28 00:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (97.46%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+      int n = nums.size();
+
+      int low = 0;
+      int mid = 0;
+      int high = n-1;
+
+      while(mid <= high){
+        if(nums[mid] == 0){
+            int temp = nums[mid];
+            nums[mid] = nums[low];
+            nums[low] = temp;
+
+            low++;
+            mid++;
+        }
+        else if(nums[mid] == 1){
+            mid++;
+        }
+        else
+        {
+            int temp = nums[high];
+            nums[high] = nums[mid];
+            nums[mid] = temp;
+     
+            high--;
+        }
+      } 
+    }
+};
