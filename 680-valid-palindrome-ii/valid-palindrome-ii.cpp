7f14bde1beb7c9914ea05cb7185c9f3139618f34
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-05-28 19:43:29 +0530

    Time: 4 ms (58.79%) | Memory: 26.7 MB (28.98%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    bool isPalindrome(string s,int l,int r){
+        while(l<r){
+
+            if(s[l] != s[r]){
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+
+        int n = s.length();
+
+        int l = 0;
+        int r = n-1;
+
+        while(l<r){
+            if(s[l] == s[r]){
+                l++;
+                r--;
+            }
+            else
+            {
+                return isPalindrome(s,l+1,r) ||
+                        isPalindrome(s,l,r-1);
+            }
+        }
+
+        return true;
+        
+    }
+};
