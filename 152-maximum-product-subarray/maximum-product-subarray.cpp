5c4bb94da50a27303829c0ba962b056433c1e6f8
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-06-05 10:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (58.93%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -5,26 +5,23 @@ public:
         int n = nums.size();
 
         int pre = 1;
-        int suff = 1;
+        int suf = 1;
 
         int res = nums[0];
+
         for(int i=0;i<n;i++){
-
-
-            if(pre == 0){
-                pre = 1;
-            }
-            if(suff == 0){
-                suff = 1;
-            }
+            
+            if(pre == 0) pre = 1;
+            if(suf == 0) suf = 1;
 
             pre *= nums[i];
-            suff *= nums[n-i-1];
+            suf *= nums[n-i-1];
 
-            res = max(res,max(pre,suff));
+            res = max(res,max(pre,suf));
         }
-        
 
         return res;
+
+        
     }
 };
