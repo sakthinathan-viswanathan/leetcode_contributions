1fb6b08077464ebac5122cf9fde8df6b9c15c1d8
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-07-06 21:35:55 +0530

    Time: 3 ms (13.15%) | Memory: 41.9 MB (14.33%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -4,26 +4,26 @@ public:
 
         int n = nums.size();
 
-
         int sum = 0;
-
         int res = INT_MAX;
 
         int left = 0;
+        
         for(int right = 0;right<n;right++){
 
             sum += nums[right];
 
-            while(sum >= target){
-                
-                res = min(res,(right-left+1));
+            
 
-                    sum -= nums[left];
-                    left++;
+            while(sum >= target){
+                res = min((right-left+1),res);
+                sum -= nums[left];
+                left++;
             }
 
         }
+
+        return res==INT_MAX ?0:res;
         
-        return res == INT_MAX?0:res;
     }
 };
