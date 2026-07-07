866119c23856b441cc331b589fdfaf904526b38e
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-07-07 20:20:08 +0530

    Time: 10 ms (31.58%) | Memory: 11.2 MB (14.81%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,39 +2,45 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
 
-        int n = s2.length();
-        int k = s1.length();
+        int n = s1.size();
+        int m = s2.size();
 
-        unordered_map<char, int> mp;
+        unordered_map<char,int>mp;
 
-        for (char c : s1) {
+        for(char c:s1){
             mp[c]++;
         }
 
+//        unordered_map<int,int>freq;
         int left = 0;
-        for (int right = 0; right < n; right++) {
 
+        for(int right = 0;right<m;right++){
+
+           
             mp[s2[right]]--;
 
-            if (mp[s2[right]] == 0) {
+            if(mp[s2[right]] == 0){
                 mp.erase(s2[right]);
             }
 
-            if (right - left + 1 == k) {
+           if(mp.size() == 0){
+            return true;
+           }
 
-                if (mp.empty()) {
-                    return true;
-                }
-
+           while((right-left+1) >= n){
                 mp[s2[left]]++;
 
-                if (mp[s2[left]] == 0) {
+                if(mp[s2[left]] == 0){
                     mp.erase(s2[left]);
                 }
 
                 left++;
-            }
+           }
+
         }
+
+
         return false;
+        
     }
 };
