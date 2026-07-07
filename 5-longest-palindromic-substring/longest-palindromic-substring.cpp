b2c8e2fb95f34639f94ebbf753e17c9fbf1f1f42
SAKTHINATHAN V <sakthinathan3110@gmail.com>
2026-07-07 12:06:53 +0530

    Time: 89 ms (31.21%) | Memory: 174.2 MB (13.62%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,42 +1,48 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
+    string checkpalindrome(string s,int l,int r){
 
-        int n = s.length();
+        int n = s.size();
 
-        if(s.length() == 0 || s == ""){
-            return "";
-        }
-        
-        string ans = "";
-
-        for(int i=0;i<n;i++){
-            
-            string even = isPalindrome(i,i,s);
-            string odd = isPalindrome(i,i+1,s);
-
-            if(ans.size() < even.size()){
-                ans = even;
+        while(l >= 0 && r < n ){
+            if(s[l] != s[r]){
+                break;
             }
 
-            if(ans.size() < odd.size()){
-                ans = odd;
-            }
-
-        }
-
-        return ans;
-    }
-
-    string isPalindrome(int l,int r,string s){
-
-        
-
-        while(l >= 0 && r < s.size() && s[l] == s[r]){
             l--;
             r++;
         }
 
         return s.substr(l+1,r-l-1);
+    }
+    string longestPalindrome(string s) {
+
+        int n = s.size();
+
+        string res = "";
+
+        if(s.size() <= 1){
+            return s;
+        }
+
+        for(int i=0;i<n-1;i++){
+
+            string odd = checkpalindrome(s,i,i);
+
+            string even = checkpalindrome(s,i,i+1);
+
+            if(res.size() < odd.size()){
+                res = odd;
+            }
+
+            if(res.size() < even.size()){
+                res = even;
+            }
+
+        }
+
+
+        return res;
+        
     }
 };
