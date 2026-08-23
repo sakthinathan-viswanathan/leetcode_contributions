08ff9765830d1f2f289fefd163454d8356235a96
SAKTHINATHAN V <sakthiviswa61@gmail.com>
2026-08-24 00:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (98.68%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,20 +4,22 @@ public:
 
         int n = nums.size();
 
-        int sum = 0;
         int res = nums[0];
 
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-            
-            res = max(sum, res);
+        int sum = 0;
 
+        for(int i=0;i<n;i++){
+
+            sum += nums[i];
+
+            res = max(res,sum);
+            
             if(sum < 0){
                 sum = 0;
             }
 
         }
-
+        
         return res;
         
     }
