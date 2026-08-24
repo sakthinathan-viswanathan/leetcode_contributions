51e137d954211a74dea515df94e3c84de73095cb
SAKTHINATHAN V <sakthiviswa61@gmail.com>
2026-08-24 23:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 113.9 MB (27.91%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -4,26 +4,28 @@ public:
 
         int n = nums.size();
 
-        double ans = INT_MIN;
-        int left = 0;
-
+        double res = INT_MIN;
         double sum = 0;
+
+        int left = 0;
 
         for(int right = 0;right<n;right++){
 
             sum += nums[right];
 
-            if((right - left +1) == k){
-                double avg = sum / k;
+            if(right - left +1 == k){
+                double avg = (sum / k);
 
-                ans = max(ans,avg);
+                res = max(res,avg);
 
                 sum -= nums[left];
+
                 left++;
             }
+
         }
 
-        return ans;
+        return res;
         
     }
 };
