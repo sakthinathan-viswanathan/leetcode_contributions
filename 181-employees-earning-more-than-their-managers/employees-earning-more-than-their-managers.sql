6e5f862ea84805b5644e9aebb85dbf4df014df15
SAKTHINATHAN V <sakthiviswa61@gmail.com>
2026-09-22 16:41:34 +0530

    Time: 489 ms (24.94%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
 
 select e1.name as Employee
-    from employee e1 
-    inner join 
-    employee e2 on 
-    e1.managerId = e2.id  and e1.salary > e2.salary;
+from Employee e1
+inner join Employee e2
+on e1.managerId = e2.id && e1.salary > e2.salary;
